7f6a2f511039184e42f2bc34bc3f03dbd6999d77
WgrWebDev <wgrwebdev@gmail.com>
2021-01-23 13:47:01 -0600

    Update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,6 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  version: require('../package.json').version,
   production: false,
   access: 'beta',
   testnet: {
