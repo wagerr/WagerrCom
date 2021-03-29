1e718bc7af1199706cad296797038ba1561677e4
WgrWebDev <wgrwebdev@gmail.com>
2021-03-29 12:44:47 -0500

    add index.php to the build process to support the bet system

--- src/index.php
+++ src/index.php
@@ -5,16 +5,15 @@
   <title>Wagerr — The private, no limit sportsbook</title>
   <base href="/">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="icon" type="image/x-icon" href="assets/images/wgrmodernicon-01.png">
-  <script src="https://kit.fontawesome.com/575d0f7d02.js" crossorigin="anonymous"></script>
-//   <meta name="twitter:card" content="summary_large_image">
-//   <meta name="twitter:site" content="/sportsbook">
-//   <meta name="twitter:creator" content="@betlikeaninja">
-//   <meta name="twitter:title" content="I Bet 1000 WGR on Norte Dame to Win">
-//   <meta name="twitter:description" content="Notre Dame vs Florida State">
-  <meta name="twitter:image" content="https://img.wagerr.com/hotprice.php?type=moneyline&select=draw&hometeam=Notre%20Dame&awayteam=Florida&title=-17.5&price=20.01&boost=1.25&view=true">
+  <link rel="icon" type="image/x-icon" href="../assets/images/wgrmodernicon-01.png">
+  <meta name="twitter:card" content="summary_large_image" />
+  <meta name="twitter:site" content="@wagerrx" />
+  <meta name="twitter:title" content="Wagerr - The private, no limit sportsbook">
+  <meta name="twitter:description" content="The private, no limit sportsbook">
+  <meta name="twitter:image" content="https://img.wagerr.com/myBet.php?<?php echo $_SERVER['QUERY_STRING']; ?>">
+  <meta name="og:image" content="https://img.wagerr.com/myBet.php?<?php echo $_SERVER['QUERY_STRING']; ?>">
+<meta http-equiv="refresh" content="2;url=https://wagerr.com/?<?php echo $_SERVER['QUERY_STRING']; ?>" />
 </head>
-<body class="bodyStart">
-  <app-root></app-root>
+<body>
 </body>
 </html>
