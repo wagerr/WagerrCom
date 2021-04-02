b634e9e7f174e1f89f40b9e6b269f0b4b3158989
WgrWebDev <wgrwebdev@gmail.com>
2021-04-02 18:50:15 -0500

    Update to fix Event Twitter Post

--- src/app/service/Corefunc.ts
+++ src/app/service/Corefunc.ts
@@ -3,14 +3,14 @@ import {environment} from '../../environments/environment';
 export class Corefunc {
   private version = environment[environment.access].ver;
 
-  getMLPoints(item: any, type: string): any {
+  getMLPoints(item: any, type: string, show = true): any {
     if (item.odds[0]['ml' + type]) {
       return this.updateTrueOdds((item.odds[0]['ml' + type] / 10000));
     }
     return 0;
   }
 
-  getSpreadNumber(item: any, type: any): any {
+  getSpreadNumber(item: any, type: any, show = true): any {
     if (item.odds[1].spreadHome > 0 || item.odds[1].spreadAway > 0) {
       if (item.odds[1].spreadPoints || (this.version === 2 && item.odds[1].spreadPoints >= 0)) {
         const set = (item.odds[1].favorite === type) ? '-' : '+';
@@ -29,24 +29,35 @@ export class Corefunc {
         return set + (item.odds[1].spreadPoints / 10);
       }
     }
-    return '—';
+
+    if (show) {
+      return '—';
+    }
+    return 0;
   }
 
-  getSpreadPoints(item: any, type: string): any {
+  getSpreadPoints(item: any, type: string, show = true): any {
     if ((item.odds[1].spreadPoints || (this.version === 2 && item.odds[1].spreadPoints >= 0)) && item.odds[1]['spread' + type]) {
       return this.updateTrueOdds((item.odds[1]['spread' + type] / 10000));
     }
-    return '—';
+
+    if (show) {
+      return '—';
+    }
+    return 0;
   }
 
-  getTotalsNumber(item: any): any {
+  getTotalsNumber(item: any, show = true): any {
     if (item.odds[2].totalsPoints) {
       if (this.version === 2) {
         return (item.odds[2].totalsPoints / 100);
       }
       return (item.odds[2].totalsPoints / 10);
     }
-    return '—';
+    if (show) {
+      return '—';
+    }
+    return 0;
   }
 
   getTotalsPoints(item: any, type: string, show = true): any {
